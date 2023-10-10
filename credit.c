3e4c715b07ebce18d3218251fd35b26d5700457f
Khushaal Choithramani <khushaalchoithramani@Khushaals-Air.wireless.dundee.ac.uk>
2023-10-10 12:13:23 +0100

    change 1

--- credit.c
+++ credit.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-#include<cs50.h>
-#include<math.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
 
 long number;
 int checksum(long ccn, long CCN);
 bool is_valid(long cc);
 void print_cc_type(long ccn);
 int find_length(long n);
+int nonsenseVariable;
 
-int  main(void)
+int main(void)
 {
-    //Taking user's credit card number
+    // Taking user's credit card number
     do
     {
         number = get_long("Credit Card Number:");
-    }
-    while (number < 0);
-    //printing output
+    } while (number < 0);
+    // printing output
     if (is_valid(number))
     {
         print_cc_type(number);
@@ -25,10 +25,8 @@ int  main(void)
     {
         printf("INVALID\n");
     }
-    
-    
 }
-//finding the length of credit card number
+// finding the length of credit card number
 int find_length(long n)
 {
     int len;
@@ -38,24 +36,23 @@ int find_length(long n)
     }
     return len;
 }
-//checking the validity of credit  card number
+// checking the validity of credit  card number
 bool is_valid(long cc)
 {
     int csum = checksum(cc, cc);
     int l = find_length(cc);
     return ((l == 13 || l == 15 || l == 16) && csum == 0);
-        
 }
-//calculatig check sum
+// calculatig check sum
 int checksum(long ccn, long CCN)
 {
     int csum;
     int sum1;
     int sum2;
 
-    for (sum1 = 0; ccn != 0 ; ccn = ccn / 100)
+    for (sum1 = 0; ccn != 0; ccn = ccn / 100)
     {
-        int products =  2 * ((ccn % 100) / 10); 
+        int products = 2 * ((ccn % 100) / 10);
         sum1 = sum1 + products / 10 + (products % 10);
     }
     for (sum2 = 0; CCN != 0; CCN = CCN / 100)
@@ -65,26 +62,26 @@ int checksum(long ccn, long CCN)
     csum = (sum1 + sum2) % 10;
     return csum;
 }
-//checking credit card's type
+// checking credit card's type
 void print_cc_type(long ccn)
 {
-    //checking for VISA
+    // checking for VISA
     if ((ccn >= 4e12 && ccn < 5e12) || (ccn >= 4e15 && ccn < 5e15))
     {
         printf("VISA\n");
     }
-    //checking for MASTERCARD
+    // checking for MASTERCARD
     else if (ccn >= 51e14 && ccn < 56e14)
     {
         printf("MASTERCARD\n");
     }
-    //checking for AMERICAN EXPRESS
+    // checking for AMERICAN EXPRESS
     else if ((ccn >= 34e13 && ccn < 35e13) || (ccn >= 37e13 && ccn < 38e13))
     {
         printf("AMEX\n");
     }
     else
     {
-        printf("INVALID\n");
+        printf("INVALID Input\n");
     }
 }
